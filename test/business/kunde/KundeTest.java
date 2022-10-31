4ae28dacaa2e86c46496819c5b9279d59c625858
Jan Krueger <jankrueger1999@gmail.com>
2022-10-31 21:42:01 +0100

    ci: add test workflow

---
+++ test/business/kunde/KundeTest.java
@@ -0,0 +1,15 @@
+package business.kunde;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class KundeTest {
+
+    @Test
+    void testHausnummer() {
+        Kunde kunde = new Kunde();
+        kunde.setHausnummer(1);
+        assertEquals(1, kunde.getHausnummer());
+    }
+}
